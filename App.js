b0697bc18e056482d98d50fd9a4aa30df81cc4e0
Tony Choi <tchoi52@gmail.com>
2020-08-25 19:16:57 -0400

    Fix(Components): fixed the syntaxes Created the login component Changed the app.js function to a component

--- App.js
+++ App.js
@@ -1,30 +1,51 @@
 import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {Component} from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { StyleSheet, Text, View } from 'react-native';
+import Login from './Components/Authentication/Login'
 
 
-function Login() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Log In</Text>
-    </View>
-  );
-}
+// function Login() {
+//   return (
+//     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+//       <Text>Log In</Text>
+//     </View>
+//   );
+// }
 
 const Stack = createStackNavigator();
 
-function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Login" component={Login} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+// function App() {
+//   return (
+//     <NavigationContainer>
+//       <Stack.Navigator>
+//         <Stack.Screen name="Login" component={Login} />
+//       </Stack.Navigator>
+//     </NavigationContainer>
+//   );
+// }
+
+class App extends Component {
+
+
+  componentWillMount() {
+    //function here
+  }
+
+  render() {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Login" component={Login} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
+  
 }
+
 
 
 
---
+++ Components/Authentication/Login.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+
+
+
+class Login extends Component {
+
+  render() {
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Log In</Text>
+      </View>
+    );
+  }
+  
+}
+
+
+export default Login
