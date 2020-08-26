20fe122d3cd702ed3b319d8fefd19199eba31b45
dyermakou3 <69915218+dyermakou3@users.noreply.github.com>
2020-08-26 18:12:30 -0400

    Added input fields for user info

--- Components/Authentication/Register.js
+++ Components/Authentication/Register.js
@@ -1,33 +1,63 @@
 import React, {Component} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import {Button} from 'native-base'
-
+import {Button} from 'react-native';
+import {TextInput} from 'react-native';
+import * as firebase from 'firebase';
 
 
 class Register extends Component {
 
-  render() {
+	state = {
+	    firstName: '',
+	    lastName: '',
+	    email: '',
+	    password: '',
+	    /*committe: ?? not sure*/
+	  }
+
+	  
+
+
+ 	render() {
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Register Me!</Text>
-        <Button> Register </Button> 
-        /*////onPress={() => this.onPressRegister()}><Text style={styles.text}>Register</Text></Button>*/
+	        <Text>Enter your info to register.</Text>
+
+	        {/*<Text>Enter your First Name</Text>*/}
+            <TextInput
+                autoCorrect={false}
+                onChangeText={firstName => this.setState({firstName})}
+                placeholder={'Enter your First Name'}
+                value={this.state.firstName}
+            />            
+            <TextInput
+                autoCorrect={false}
+                onChangeText={lastName => this.setState({lastName})}
+                placeholder={'Enter your Last Name'}
+                value={this.state.lastName}
+            />
+            <TextInput
+                autoCorrect={false}
+                onChangeText={email => this.setState({email})}
+                placeholder={'Enter your email'}
+                value={this.state.email}
+            />
+            <TextInput
+                autoCorrect={false}
+                onChangeText={password => this.setState({password})}
+                placeholder={'Enter your password'}
+                value={this.state.password}
+            />
+
+
+	        <Button color="blue" title="Register"> </Button> 
+	        {/*////onPress={() => this.onPressRegister()}><Text style={styles.text}>Register</Text></Button>*/}
       
     	</View>
     );
   }
   
 
-// const styles = StyleSheet.create({
-//   button: {
-//     marginTop: 10,
-//     padding: 20,
-//     width: '100%',
-//     backgroundColor: '#00eeef',
-//     borderRadius: 10,
-//     alignItems: 'center'
-//   }
-// });
 }
 
 
