6d35da4e463476a1d4ab68ebc062809835322e95
dyermakou3 <69915218+dyermakou3@users.noreply.github.com>
2020-09-02 19:13:22 -0400

    Register should work now
    Didn't tamper with dependencies this time around

--- Components/Authentication/Register.js
+++ Components/Authentication/Register.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { StyleSheet, Text, View, Alert, TextInput } from 'react-native';
+import { StyleSheet, Text, View, Alert, TextInput, Picker } from 'react-native';
 import {Button} from 'native-base';
 
 const firebase = require("../../server/router");
@@ -14,6 +14,10 @@ class Register extends Component {
 	    group: '',
 	  }
 
+  	updateGroup = (group) => {
+        this.setState({ group:group })
+    }
+
 	onPressRegister() {
 		firebase.firebaseConnection.firestore().collection('PendingUsers').doc(this.state.email).set({
 	        firstName: this.state.firstName,
@@ -22,7 +26,7 @@ class Register extends Component {
 	        Committee: this.state.group
 	    }).then((data) => {
 	        console.log("added customer")
-	        Alert.alert("Your request to register has been made successfully");
+	        Alert.alert("Your request to register has been made successfully.");
 			var navigation = this.props.navigation;
 			navigation.navigate('Login')	        
 	    }).catch((error) => {
@@ -39,40 +43,60 @@ class Register extends Component {
  	render() {
     return (
         <View style={styles.container}>
-	        <Text>Enter your info to register.</Text>
+	        <View style={styles.form}>
+		        <Text>Enter your info to register.</Text>
 
-            <TextInput
-                autoCorrect={false}
-                onChangeText={firstName => this.setState({firstName})}
-                placeholder={'Enter your First Name'}
-                value={this.state.firstName}
-            />            
-            <TextInput
-                autoCorrect={false}
-                onChangeText={lastName => this.setState({lastName})}
-                placeholder={'Enter your Last Name'}
-                value={this.state.lastName}
-            />
-            <TextInput
-                autoCorrect={false}
-                onChangeText={email => this.setState({email})}
-                placeholder={'Enter your email'}
-                value={this.state.email}
-            />
-            <TextInput
-                autoCorrect={false}
-                onChangeText={password => this.setState({password})}
-                placeholder={'Enter your password'}
-                value={this.state.password}
-            />
+	            <TextInput style = {styles.input}
+	                autoCorrect={false}
+	                onChangeText={firstName => this.setState({firstName})}
+	                placeholder={'Enter your First Name'}
+	                value={this.state.firstName}
+	            />            
+	            <TextInput style = {styles.input}
+	                autoCorrect={false}
+	                onChangeText={lastName => this.setState({lastName})}
+	                placeholder={'Enter your Last Name'}
+	                value={this.state.lastName}
+	            />
+	            <TextInput style = {styles.input}
+	                autoCorrect={false}
+	                onChangeText={email => this.setState({email})}
+	                placeholder={'Enter your email'}
+	                value={this.state.email}
+	            />
+	            <TextInput style = {styles.input}
+	                autoCorrect={false}
+	                onChangeText={password => this.setState({password})}
+	                placeholder={'Enter your password'}
+	                value={this.state.password}
+	            />
 
-            {/* Debug <Text>Testing state works: {this.state.firstName + " " + this.state.group}</Text> */}
+	            <Picker
+		            selectedValue={this.state.group}
+		            style={styles.picker}
+		            onValueChange={this.updateGroup}
+		          >
+		            <Picker.Item label="Pick a board to join" value="" />
+		            <Picker.Item label='Budget, Finance, & Audit' value="budget" />
+		            <Picker.Item label="Board Development" value="boardDevelopment" />
+		            <Picker.Item label="Executive" value="exec" />
+		            <Picker.Item label="Human Resources" value="humanResources" />
+		            <Picker.Item label="Impact & Investment" value="impact" />
+		            <Picker.Item label="Resource Development & Marketing" value="marketing" />
+		            <Picker.Item label="Safety Asset Management" value="safety" />
+		        </Picker>
 
-		    <View style={styles.form1}>
-		        <Button  style={styles.button} onPress={() => this.onPressRegister()}><Text style={styles.text}>Register</Text></Button>
-            	<Button  style={styles.button} onPress={() => this.onPressCancel()}><Text style={styles.text}>Cancel</Text></Button>
-		        <Text>  </Text>
-    		</View>
+	            {/* Debug <Text>Testing state works: {this.state.firstName + " " + this.state.group}</Text> */}
+
+			    <View style={styles.buttonHolder}>
+			        <Button  style={styles.button} onPress={() => this.onPressRegister()}>
+			        	<Text style={styles.text}>Register</Text>
+		        	</Button>
+	            	<Button  style={styles.button2} onPress={() => this.onPressCancel()}>
+	            		<Text style={styles.text}>Cancel</Text>
+            		</Button>
+	    		</View>
+	    	</View>
     	</View>
     );
     }
@@ -84,7 +108,11 @@ const styles = StyleSheet.create({
       alignItems: 'center', 
       justifyContent: 'center',
     },
-    form1: {
+    form: {
+	  flex: 1,
+	  marginTop: '10%',
+	},
+    buttonHolder: {
       flex: 0,
       flexDirection: 'row',
       justifyContent: 'center',
@@ -101,9 +129,37 @@ const styles = StyleSheet.create({
       marginRight: 10,
       marginLeft: 10,
     },
-      dropDownContainer: {
+    button2: {
+      flexDirection: 'row', 
+      padding: 20,
+      width: '40%',
+      height: '180%',
+      backgroundColor: 'red',
+      borderRadius: 35,
+      alignItems: 'center',
+      marginRight: 10,
+      marginLeft: 10,
+    },
+    text: {
+      color: 'white',
+      fontWeight: '700',
+      fontSize: 20,
+      marginRight: 'auto',
+      marginLeft: 'auto'
+    },
+    picker: {
       height: 50, 
-      width: 300,
+      borderColor: '#7a42f4',
+      borderWidth: 1,
+      marginBottom: 15
+    },
+    input: {
+       margin: 15,
+       marginLeft: 2,
+       height: 40,
+       borderColor: 'black',
+       borderWidth: 1,
+       paddingLeft: 10
     },
 });
 
