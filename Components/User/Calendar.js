6f8485763f2b258cc311f1cc08384511a1ae3ba2
Tony Choi <tchoi52@gmail.com>
2020-09-06 16:20:33 -0400

    feat(TeamsPage): created pages for teams

--- Components/User/Calendar.js
+++ Components/User/Calendar.js
@@ -5,7 +5,23 @@ import { StyleSheet, Text, View } from 'react-native';
 
 
 class Calendar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: null,
+      committee: null,
+      admin: false,
+      executive: false,
+      selectedCommittee: null,
+    }
+  }
 
+  componentDidMount() {
+    var state = this.props.route.params.state
+    Object.keys(state).forEach(key => {
+      this.setState({[key]: state[key]})
+    });
+  }
   render() {
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
--- Components/User/Pages.js
+++ Components/User/Pages.js
@@ -1,15 +1,61 @@
 import React, {Component} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Button } from 'native-base'
 
+const s = require('../../Style/style')
+const styles = s.styles
 
 
 
 class Pages extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: null,
+      committee: null,
+      admin: false,
+      executive: false,
+    }
+  }
+
+  componentDidMount() {
+    var state = this.props.route.params.state
+    Object.keys(state).forEach(key => {
+      this.setState({[key]: state[key]})
+    });
+
+    
+
+  }
+  getCommittees() {
+    if(this.state.committee){
+      return(
+        <View style={styles.pageButtonHolder}>
+          <Button  style={styles.pageButton} onPress={() => this.goToPage(this.state.committee)}><Text style={styles.text}>{this.state.committee}</Text></Button>
+        </View>
+      )
+    }
+  }
+  getExec() {
+    if(this.state.executive){
+      return(
+        <View style={styles.pageButtonHolder}>
+          <Button  style={styles.pageButton} onPress={() => this.goToPage('Executive Committee')}><Text style={styles.text}>Executive Committee</Text></Button>
+        </View>
+      )
+    }
+  }
+  goToPage(selected) {
+    var navigation = this.props.navigation;
+    navigation.navigate('Team Page', {state: this.state, selectedCommittee: selected})
+  }
+
   render() {
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Pages</Text>
+      <View style={{ flex: 1, alignItems: 'center', marginTop:'20%'}}>
+        {this.getCommittees()}
+        {this.getExec()}
       </View>
     );
   }
