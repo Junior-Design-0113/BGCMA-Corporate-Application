e93cf9db26f157a9df808392e80c8b16a98027df
Ilya Velmiskin <ilya@gatech.edu>
2020-09-09 15:19:20 -0400

    added working Upload button to Meeting Files page

--- Components/CommitteePage/MeetingFiles.js
+++ Components/CommitteePage/MeetingFiles.js
@@ -1,10 +1,11 @@
 import React, {Component} from 'react';
 import { ActivityIndicator, StyleSheet, Text, View, Alert, Image } from 'react-native';
 import { Button, Row } from 'native-base'
+import * as DocumentPicker from 'expo-document-picker';
 
+const fb = require("../../server/router");
 const s = require('../../Style/style')
 const styles = s.styles
-
 
 class MeetingFiles extends Component {
   constructor(props) {
@@ -32,11 +33,41 @@ class MeetingFiles extends Component {
     }
   }
 
+  getFile() {
+    DocumentPicker.getDocumentAsync().then((res) => {
+      if (res.type === 'success') {
+        Alert.alert("Confirm File Upload?", res.name,
+        [{text: "Cancel", onPress: () => console.log("Cancel")},
+        {text: "Upload", onPress: () => this.uploadFile(res)}]) 
+        console.log(res.name)
+      } else {
+        Alert.alert('No file was selected')
+      }
+    })
+  }
+
+  uploadFile = async(res) => {
+    var storageRef = fb.firebaseConnection.storage().ref()
+    
+    //If greater than 1MB (size is in bytes), cancel
+    if (res.size > 1000000) {
+      Alert.alert("File is too large to upload")
+      return
+    }
+    const response = await fetch(res.uri);
+    const blob = await response.blob();
+    var ref = storageRef.child(this.state.committee + "/" + res.name);
+    return ref.put(blob);
+  }
+
   render() {
       return (
         <View style={styles.container}>
           <View style={styles.form}>
           {this.getTeam()}
+          <View style={styles.pageButtonHolder}>
+            <Button  style={styles.pageButton} onPress={() => this.getFile()}><Text style={styles.text}>+</Text></Button>
+          </View>
           </View>
         </View>
       )
