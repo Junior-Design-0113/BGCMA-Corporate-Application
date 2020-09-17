0547d9b2c57a3a194f6ff92d8fa6c7ea466b38f2
Tony Choi <tchoi52@gmail.com>
2020-09-17 08:59:03 -0400

    feat(uploadfile): modal with custom file name

--- Style/style.js
+++ Style/style.js
@@ -111,10 +111,59 @@ module.exports.styles = StyleSheet.create({
       },
       searchBarText: {
         fontSize: 20,
+        marginTop:0
       },
       searchText: {
         fontSize: 20,
         marginTop: 5,
         marginBottom: 5,
+      },
+      centeredView: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: 22
+      },
+      modalView: {
+        // margin: 20,
+        width: '80%',
+        backgroundColor: "white",
+        borderRadius: 20,
+        padding: 35,
+        alignItems: "center",
+        shadowColor: "#000",
+        shadowOffset: {
+          width: 0,
+          height: 2
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 5
+      },
+      openButton: {
+        backgroundColor: "#2196F3",
+        borderRadius: 20,
+        padding: 10,
+        elevation: 2
+      },
+      modalInput: {
+        width: '80%',
+        margin: 15,
+        height: 40,
+        borderColor: 'black',
+        borderWidth: 1,
+        paddingLeft: 10
+      },
+      uploadCancelButton: {
+        flexDirection: 'row', 
+        padding: 10,
+        width: '40%',
+        height: '180%',
+        backgroundColor: '#0081c6',
+        borderRadius: 35,
+        alignItems: 'center',
+        marginRight: 10,
+        marginLeft: 10,
+        borderRadius: 20, 
       }
     });
