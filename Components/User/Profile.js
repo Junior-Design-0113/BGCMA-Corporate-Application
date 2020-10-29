bbce721c0c786fbc4e40b93e22c2b2f10125b46d
annagho <annaho287@gmail.com>
2020-10-29 02:10:44 -0400

    Edit Prof Pic

--- Components/User/Profile.js
+++ Components/User/Profile.js
@@ -1,20 +1,47 @@
-import React, {Component} from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component, useState, useEffect } from 'react';
+import { StyleSheet, Text, View, Button, Image, Platform} from 'react-native';
 
-
+import * as ImagePicker from 'expo-image-picker';
+import Constants from 'expo-constants';
+import { render } from 'react-dom';
 
 
 class Profile extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          imageURL: '',
+          URL: []
+        }
+      }
+
+    pickImage = async () => {
+        global.imageURL = 'https://reactnativecode.com/wp-content/uploads/2017/05/react_thumb_install.png'; 
+        let result = await ImagePicker.launchImageLibraryAsync({
+          mediaTypes: ImagePicker.MediaTypeOptions.All,
+          allowsEditing: true,
+          aspect: [4, 3],
+          quality: 1,
+        });
+    
+        console.log(result);
+    
+        if (!result.cancelled) {
+            this.setState({imageURL: (result.uri)});
+        }
+    };
 
     render() {
-    return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Profile</Text>
-        </View>
-    );
+        return (
+            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+              <Button title="Edit Image" onPress={this.pickImage} />
+                <Image source={ {uri: this.state.imageURL} } style={{ width: 200, height: 200 }} />   
+            </View>
+          );
     }
-  
+
+
 }
-
-
 export default Profile
+
+
