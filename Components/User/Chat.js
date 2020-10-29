f41e73580a9fd766f360905d37b74dcfb00e2021
Tony Choi <tchoi52@gmail.com>
2020-10-29 15:15:18 -0400

    feat: Sending messages to db. Not finished

--- Components/User/Chat.js
+++ Components/User/Chat.js
@@ -1,20 +1,46 @@
 import React, {Component} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
-
+import { GiftedChat } from 'react-native-gifted-chat';
+import Fire from './Chat/Fire';
 
 
 class Chat extends Component {
-
-  render() {
-    return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Chat</Text>
-      </View>
+  componentDidMount() {
+    Fire.shared.on(message =>
+      this.setState(previousState => ({
+        messages: GiftedChat.append(previousState.messages, message),
+      }))
     );
   }
-  
+  // 2.
+  componentWillUnmount() {
+    Fire.shared.off();
+  }
+  get user() {
+    // Return our name and our UID for GiftedChat to parse
+    return {
+      name: "Name",
+      _id: Fire.shared.uid,
+    };
+  }
+  static navigationOptions = ({ navigation }) => ({
+    title: (navigation.state.params || {}).name || 'Chat!',
+  });
+  state = {
+    messages: [],
+  };
+ 
+  render() {
+    return (
+      <GiftedChat
+        messages={this.state.messages}
+        onSend={Fire.shared.send}
+        user={this.user}
+      />
+    );
+  }
 }
+export default Chat;
 
 
-export default Chat
+
---
+++ Components/User/Chat/Fire.js
@@ -0,0 +1,90 @@
+import firebase from 'firebase'; 
+class Fire {
+  constructor() {
+    this.init();
+    this.observeAuth();
+  }
+  
+  init = () =>
+    firebase.initializeApp({
+        apiKey: "AIzaSyBxdS6aapWbOthR72uEFe_sJmn4vaQeN08",
+        authDomain: "bgcma-corporate-portal.firebaseapp.com",
+        databaseURL: "https://bgcma-corporate-portal.firebaseio.com",
+        projectId: "bgcma-corporate-portal",
+        storageBucket: "bgcma-corporate-portal.appspot.com",
+        messagingSenderId: "195535537984",
+        appId: "1:195535537984:web:5138ec640e9be03ead5ca5",
+        measurementId: "G-5J9ZZWT81S"
+    });
+
+    observeAuth = () => firebase.auth().onAuthStateChanged(this.onAuthStateChanged);
+
+    onAuthStateChanged = user => {
+        if (!user) {
+          try {
+            // 4.
+            firebase.auth().signInAnonymously();
+          } catch ({ message }) {
+            alert(message);
+          }
+        }
+      };
+
+    // 1.
+    get ref() {
+        return firebase.database().ref('messages');
+    }
+    // 2.
+    on = callback =>
+        this.ref
+            .limitToLast(20)
+            .on('child_added', snapshot => callback(this.parse(snapshot)));
+    // 3.
+    parse = snapshot => {
+        // 1.
+        const { timestamp: numberStamp, text, user } = snapshot.val();
+        const { key: _id } = snapshot;
+        // 2.
+        const timestamp = new Date(numberStamp);
+        // 3.
+        const message = {
+            _id,
+            timestamp,
+            text,
+            user,
+        };
+        return message;
+    }
+    // 1.
+    get uid() {
+        return (firebase.auth().currentUser || {}).uid;
+    }
+    // 2.
+    get timestamp() {
+        return firebase.database.ServerValue.TIMESTAMP;
+    }
+    
+    // 3.
+    send = messages => {
+        for (let i = 0; i < messages.length; i++) {
+        const { text, user } = messages[i];
+        // 4.
+        const message = {
+            text,
+            user,
+            timestamp: this.timestamp,
+        };
+        this.append(message);
+        }
+        console.log(messages)
+    };
+    // 5.
+    append = message => this.ref.push(message);
+    // 4.
+    off() {
+        this.ref.off();
+    }
+
+}
+Fire.shared = new Fire()
+export default Fire
