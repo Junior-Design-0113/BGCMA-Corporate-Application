58d42db8618a907729c7e884fa16e902050c4f92
annagho <annaho287@gmail.com>
2020-11-18 02:24:42 -0500

    List Chat Rooms

--- Components/User/ChatPage.js
+++ Components/User/ChatPage.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 import {Text, StyleSheet, TextInput, View, Alert, Switch} from 'react-native';
 
-
 const firebase = require("../../server/router");
 const s = require('../../Style/style')
 const styles = s.styles
@@ -14,7 +13,8 @@ class ChatPage extends Component {
 	  email: null,
 	  committee: null,
 	  admin: false,
-	  executive: false,
+    executive: false,
+    chatRooms: [],
 	  users: [],
       selectedCommittee: null,
       roomId: "roomId",
@@ -32,11 +32,39 @@ class ChatPage extends Component {
     //const room = this.props.route.params.roomId;
     //this.setState({roomId: this.props.route.params.roomId})
   }
+ 
+  async getListChatRooms() {
+    const db = firebase.firebaseConnection.firestore()
+    const chatRef = await db.collection('Chat').get()
+    
+    return chatRef.docs.map(doc => doc.data());  
+  }
 
+  getUserChatRooms() {
+    const listChatRooms = this.getListChatRooms(); 
+    const chatRooms = []
+    var num = 0
+
+    listChatRooms.forEach(function(value, key) {
+      if (value === this.state.email) {
+        chatRooms.push({
+          key: num++, 
+          name: key, 
+        })
+      }
+    })
+
+    this.setState({chatRooms : chatRooms});
+    console.log(chatRooms);
+
+    return this.state.chatRooms; 
+  };
+
+   
   render() {
     return ( 
       <View style={styles.container}>
-      	<Text style={styles.title}>Chatt</Text>
+      	<Text style={styles.title}>Chat</Text>
       	<Text style={styles.title}>{"final id: " + this.state.roomId}</Text>
    	  </View>
     );
